a57b3d23aaff06d68e90628cfb2f7df6f5806937
Rajat Khandelwal <rajatgupta59@gmail.com>
2015-07-29 14:16:49 +0530

    second solution

--- tools/scripts/generate-site-public.sh
+++ tools/scripts/generate-site-public.sh
@@ -42,9 +42,9 @@ VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpres
 echo "Starting generate-site"
 CURR_BRANCH=`git branch | sed -n '/\* /s///p'`
 echo "Running site in current lens branch" $CURR_BRANCH
-mvn clean test -Dtest=org.apache.lens.doc.TestGenerateConfigDoc,org.apache.lens.cli.doc.TestGenerateCLIUserDoc -DskipCheck || die "Unable to generate config docs"
+mvn clean test -Dtest=org.apache.lens.doc.TestGenerateConfigDoc,org.apache.lens.cli.doc.TestGenerateCLIUserDoc -DskipCheck|| die "Unable to generate config docs"
 mvn install -DskipTests -DskipCheck
-mvn site site:stage -Ddependency.locations.enabled=false -Ddependency.details.enabled=false || die "unable to generate site"
+mvn site site:stage -Ddependency.locations.enabled=false -Ddependency.details.enabled=false -Pjavadoc || die "unable to generate site"
 
 echo "Site gen complete"
 
