7da898c770deaaec79a5ae3460a460d95d0b7429
Rajat Khandelwal <rajatgupta59@gmail.com>
2015-11-23 15:02:37 +0530

    end

--- lens-server/src/test/java/org/apache/lens/server/query/TestQueryConstraints.java
+++ lens-server/src/test/java/org/apache/lens/server/query/TestQueryConstraints.java
@@ -66,7 +66,7 @@ import lombok.extern.slf4j.Slf4j;
  * The Class TestQueryService.
  */
 @Slf4j
-@Test(groups = "restart-test", dependsOnGroups = "unit-test")
+@Test(groups = "two-working-drivers", dependsOnGroups = "filter-test")
 public class TestQueryConstraints extends LensJerseyTest {
   private HiveConf serverConf;
 
